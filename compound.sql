eeba0188627a4bd569b9a0142467a44513f6a2f0
techplanshetyapps <rudnikova.nataliia@gmail.com>
2026-07-30 07:29:03 +0000

    Update statements

--- compound.sql
+++ compound.sql
@@ -1,6 +1,9 @@
-CREATE TABLE (
-    CONSTRAINT CHECK ()
+CREATE TABLE TableName (
+    ReferenceID INT NOT NULL,
+    ColumnName INT NOT NULL,
+    FilterColumn INT NOT NULL,
+    CONSTRAINT CheckConstraintName CHECK (FilterColumn >= 9)
 );
 
-INSERT INTO () 
-VALUES ();
+INSERT INTO TableName (ReferenceID, ColumnName, FilterColumn) 
+VALUES (4, 20, 9);
