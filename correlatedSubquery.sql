df6c0bb09dc63760180816b635d4acb945790105
techplanshetyapps <rudnikova.nataliia@gmail.com>
2026-07-31 22:06:59 +0000

    Update query statements

--- correlatedSubquery.sql
+++ correlatedSubquery.sql
@@ -1,11 +1,10 @@
+SELECT p.ID, p.Name,
+    (SELECT AVG(s.Score) FROM custom_schema.Scores AS s WHERE s.ID = p.ID) AS AvgScore
+FROM custom_schema.Pupils AS p;
 
-SELECT
-    (SELECT AVG() FROM AS WHERE =) AS
-FROM AS;
-
-SELECT
-FROM AS
+SELECT p.ID, p.Name
+FROM custom_schema.Pupils AS p
 WHERE EXISTS (
-    SELECT FROM AS 
-    WHERE = AND >
+    SELECT 1 FROM custom_schema.Scores AS s 
+    WHERE s.ID = p.ID AND s.Score > 300
 );
