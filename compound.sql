65526e588870b508b0f8fc9a0a17d7d678151306
techplanshetyapps <rudnikova.nataliia@gmail.com>
2026-07-31 21:18:41 +0000

    Update a subquery statements

--- compound.sql
+++
@@ -1,6 +0,0 @@
-SELECT 
-FROM (
-    SELECT 
-    FROM 
-    WHERE  >= (SELECT AVG() FROM )
-) AS;
---
+++ subqueryFilterAVG.sql
@@ -0,0 +1,6 @@
+SELECT 
+FROM (
+    SELECT 
+    FROM 
+    WHERE  >= (SELECT AVG() FROM )
+) AS;
